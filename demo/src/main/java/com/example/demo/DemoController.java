900060c9d97454505e03025703c19330d631397e
Louis De Bevere <louis.debevere@kbc.be>
2019-03-20 16:01:22 +0100

    {feature} Add docker file

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/hello", produces = "text/html")
+@RequestMapping(value = "/", produces = "text/html")
 public class DemoController {
 
     @GetMapping
